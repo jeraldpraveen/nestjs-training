ebf603ee5307a3ec895d5e770cd72d29f6bbcabc
jeraldpraveen <jerald.vit@gmail.com>
2022-01-14 23:31:14 +0530

    26.Dependency Injection Implementation

--- 03-messages-app/src/messages/messages.controller.ts
+++ 03-messages-app/src/messages/messages.controller.ts
@@ -6,7 +6,9 @@ import {
   Body,
   Param,
   NotFoundException,
+  Injectable,
 } from '@nestjs/common';
+
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -14,11 +16,8 @@ import { MessagesService } from './messages.service';
 export class MessagesController {
   messagesService: MessagesService;
 
-  constructor() {
-    // Service is creating Its own dependency
-    // DON'T DO THIS ON REAL APPS
-    // USE DEPENDENCY INJECTION
-    this.messagesService = new MessagesService();
+  constructor(messagesService: MessagesService) {
+    this.messagesService = messagesService;
   }
 
   @Get()
--- 03-messages-app/src/messages/messages.service.ts
+++ 03-messages-app/src/messages/messages.service.ts
@@ -1,13 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import { MessagesRepository } from './messages.repository';
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
-
-  constructor() {
-    // Service is creating Its own dependency
-    // DON'T DO THIS ON REAL APPS
-    this.messagesRepo = new MessagesRepository();
-  }
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
