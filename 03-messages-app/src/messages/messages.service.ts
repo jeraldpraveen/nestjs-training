e9831747cc21fcecb6503b3759e19877050e09c1
jeraldpraveen <jerald.vit@gmail.com>
2022-01-14 23:40:28 +0530

    28.Extra DI tips

--- 03-messages-app/src/messages/messages.service.ts
+++ 03-messages-app/src/messages/messages.service.ts
@@ -3,7 +3,14 @@ import { MessagesRepository } from './messages.repository';
 
 @Injectable()
 export class MessagesService {
-  constructor(public messagesRepo: MessagesRepository) {}
+  constructor(
+    public messagesRepo: MessagesRepository,
+    public messagesRepo2: MessagesRepository,
+    public messagesRepo3: MessagesRepository,
+  ) {
+    console.log(messagesRepo === messagesRepo2);
+    console.log(messagesRepo2 === messagesRepo3);
+  }
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
