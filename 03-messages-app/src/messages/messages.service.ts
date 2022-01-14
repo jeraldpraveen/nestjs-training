086899bfcf160c3f41ff1195321d458e515e68e6
jeraldpraveen <jerald.vit@gmail.com>
2022-01-14 23:41:10 +0530

    29.Extra DI tips Revert

--- 03-messages-app/src/messages/messages.service.ts
+++ 03-messages-app/src/messages/messages.service.ts
@@ -3,14 +3,7 @@ import { MessagesRepository } from './messages.repository';
 
 @Injectable()
 export class MessagesService {
-  constructor(
-    public messagesRepo: MessagesRepository,
-    public messagesRepo2: MessagesRepository,
-    public messagesRepo3: MessagesRepository,
-  ) {
-    console.log(messagesRepo === messagesRepo2);
-    console.log(messagesRepo2 === messagesRepo3);
-  }
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
