62167af2a44c43feb914eff57e8a8c4717492807
jeraldpraveen <jerald.vit@gmail.com>
2022-01-14 18:15:01 +0530

    14.Generate Controller and Module with NestCLI

---
+++ 03-messages-app/src/messages/messages.controller.ts
@@ -0,0 +1,5 @@
+// Created by command[[[[nest generate controller messages/messages --flat]]]]
+import { Controller } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {}
