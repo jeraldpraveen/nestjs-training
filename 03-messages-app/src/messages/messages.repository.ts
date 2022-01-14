491a4d2f9828e7513bb7ab432e6eba22d7251542
jeraldpraveen <jerald.vit@gmail.com>
2022-01-14 20:50:57 +0530

    21.Reading and Writting Messages In file

--- 03-messages-app/src/messages/messages.repository.ts
+++ 03-messages-app/src/messages/messages.repository.ts
@@ -1,7 +1,25 @@
+import { readFile, writeFile } from 'fs/promises';
+
 export class MessagesRepository {
-  async findOne(id: string) {}
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
 
-  async create(message: string) {}
+  async create(message: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, contents: message };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
