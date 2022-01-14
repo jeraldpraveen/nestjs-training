070d8f5a37cd1149eba005063286717b11cdd571
jeraldpraveen <jerald.vit@gmail.com>
2022-01-14 14:31:42 +0530

    7.Routing DEcorators

--- 01-nest-from-scratch/src/app.controller.ts
+++ 01-nest-from-scratch/src/app.controller.ts
@@ -1,9 +1,22 @@
 import { Controller, Get } from "@nestjs/common";
 
-@Controller()
+@Controller("/app")
 export class AppController {
+  //http://localhost:3000/app/
   @Get()
   getRootRoute() {
     return "Hi,this is Jerald";
+  }
+
+  // http://localhost:3000/app/home
+  @Get("/home")
+  getHello() {
+    return "Hello Home";
+  }
+
+  // http://localhost:3000/app/bye
+  @Get("/bye")
+  getBye() {
+    return "Bye";
   }
 }
