b24148d4aca08669877288fa7b73524b8362787e
jeraldpraveen <jerald.vit@gmail.com>
2022-01-14 14:14:42 +0530

    5.Starting Up A Nest App

--- 01-nest-from-scratch/src/main.ts
+++ 01-nest-from-scratch/src/main.ts
@@ -1,4 +1,5 @@
 import { Controller, Module, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
 
 @Controller()
 class AppController {
@@ -7,3 +8,17 @@ class AppController {
     return "Hi,this is Jerald";
   }
 }
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+};
+
+bootstrap();
+
+//To Run In Terminal [[[npx ts-node-dev src/main.ts]]]
