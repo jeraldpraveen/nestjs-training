8f3b431e983a5e18428259bf41050d93893f0f1d
jeraldpraveen <jerald.vit@gmail.com>
2022-01-16 12:56:55 +0530

    48.repo.create Vs repo.save

--- 05-used-car-app/src/users/users.entity.ts
+++ 05-used-car-app/src/users/users.entity.ts
@@ -1,11 +1,18 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { AfterInsert, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   email: string;
+
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log(`Inserted user with email ${this.email}`);
+  }
 }
