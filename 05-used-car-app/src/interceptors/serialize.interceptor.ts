9b4f1007bb3ccda6f97d6cde78b1e31cbe2ca504
jeraldpraveen <jerald.vit@gmail.com>
2022-01-16 21:07:23 +0530

    57.Understand Flow Of Interceptor(Part-1)

--- 05-used-car-app/src/interceptors/serialize.interceptor.ts
+++ 05-used-car-app/src/interceptors/serialize.interceptor.ts
@@ -9,16 +9,18 @@ import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
 import { NestFactory } from '@nestjs/core';
 import { nextTick } from 'process';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // RUN SOMETHING BEFORE A REQUEST IS HANDLED BY REQUEST HANDLER IN CONTROLLER
-    console.log('Before Handler', context);
+    console.log('Before Handler');
 
     return handler.handle().pipe(
       map((data: any) => {
         // RUN SOMETHING BEFORE RESPONSE IS SENT OUT
         console.log('Before Response', data);
+        return plainToClass(UserDto, data, { excludeExtraneousValues: true });
       }),
     );
   }
