3442c5ec78fa6b6f738613be60dc86116ee80aa8
jeraldpraveen <jerald.vit@gmail.com>
2022-01-16 14:18:58 +0530

    51.Manual Testing For POST & GET Requests

--- 05-used-car-app/src/users/users.controller.ts
+++ 05-used-car-app/src/users/users.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -16,17 +17,17 @@ export class UsersController {
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+    return this.usersService.create(body.email, body.password);
   }
 
   @Get('/:id')
-  findUser(@Param() id: number) {
-    this.usersService.findOne(id);
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
   }
 
-  @Get('/:id')
-  findAllUsers(@Param() id: number) {
-    this.usersService.findOne(id);
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 
   @Patch('/:id')
